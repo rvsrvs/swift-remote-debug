57d88c30ef2e84f258e7491ba09b87d52b94d797
Van Simmons <rvs606@g.harvard.edu>
2019-02-23 15:39:35 -0500

    update build files

--- swift-runtime/arm64/build.sh
+++ swift-runtime/arm64/build.sh
@@ -1,4 +1,7 @@
-rm -rf usr lib etc
-tar xvf sdk-libs.tar.gz 
-tar xvf toolchain-libs.tar.gz 
+rm -rf swift-runtime || true
+mkdir swift-runtime
+pushd swift-runtime
+tar xvf ../runtime-libs.tar.gz
+popd
 docker build --tag cscix65g/swift-runtime:arm64-latest .
+rm -rf swift-runtime
