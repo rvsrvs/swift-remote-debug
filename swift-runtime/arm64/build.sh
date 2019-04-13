c647944b0428e29ffbbbbaf3324ea934a2096c29
Van Simmons <van.simmons@computecycles.com>
2019-04-13 10:47:57 -0400

    simplifying

--- swift-runtime/arm64/build.sh
+++ swift-runtime/arm64/build.sh
@@ -4,7 +4,8 @@ pushd swift-runtime
 tar xvf ../runtime-libs.tar.gz
 cp ../ld-2.24.so lib/aarch64-linux-gnu/ld-2.24.so
 ls -s lib/ld-linux-aarch64.so.1 lib/aarch64-linux-gnu/ld-2.24.so
-cp ../swift-ld.conf etc/ld.so.conf.d
-popd
+rm -rf etc
+cp ../Dockerfile .
 docker build --tag cscix65g/swift-runtime:arm64-latest .
+popd
 rm -rf swift-runtime
