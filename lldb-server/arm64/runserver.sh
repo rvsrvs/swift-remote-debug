8e49f755bbeb13d8b2e72e5fd35601c9a7e7040f
Van Simmons <rvsrvs@gmail.com>
2019-04-19 19:36:10 -0400

    update lldb

--- lldb-server/arm64/runserver.sh
+++ lldb-server/arm64/runserver.sh
@@ -5,4 +5,4 @@ if [ ! "$(docker ps --all -q -f name=swift_runtime)" ]; then
     docker run --name swift_runtime -d cscix65g/swift-runtime:arm64-latest
     docker logs swift_runtime
 fi
-docker run -d --rm --privileged --name lldb_server --network host -v `pwd`/debug:/debug --volumes-from swift_runtime  cscix65g/lldb-server:arm64-latest
+docker run -d --rm --privileged --name lldb_server -p 9293:9293 -p 9294:9294 -p 9295:9295 -p 9296:9296 -v /tmp:/debug --volumes-from swift_runtime  cscix65g/lldb-server:arm64-latest
