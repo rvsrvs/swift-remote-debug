1a7c8ea93882f63269b4b817462331e8488746cf
Van Simmons <rvsrvs@gmail.com>
2019-04-19 06:50:38 -0400

    adding arm64 lldb

--- lldb-server/arm64/runserver.sh
+++ lldb-server/arm64/runserver.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
-cd /debug && /usr/lib/llvm-6.0/bin/lldb-server platform --listen "*:9293" --server
+#!/bin/sh
+docker pull cscix65g/swift-runtime:arm64-latest
+if [ ! "$(docker ps --all -q -f name=swift_runtime)" ]; then
+    echo "Launching swift_runtime"
+    docker run --name swift_runtime -d cscix65g/swift-runtime:arm64-latest
+    docker logs swift_runtime
+fi
+docker run -d --rm --privileged --name lldb_server --network host -v `pwd`/debug:/debug --volumes-from swift_runtime  cscix65g/lldb-server:arm64-latest
