973b72ea3e7aa1bf7a719c9e05e4eef6cef95707
Van Simmons <rvsrvs@gmail.com>
2019-04-21 18:13:41 -0400

    more cleanup

--- swift-runtime/arm64/build.sh
+++ swift-runtime/arm64/build.sh
@@ -4,8 +4,6 @@ pushd swift-runtime
 tar xvf ../runtime-libs.tar.gz
 mkdir -p usr/bin
 cp ../helloworld usr/bin
-cp ../ld-2.24.so lib/aarch64-linux-gnu/ld-2.24.so
-ls -s lib/ld-linux-aarch64.so.1 lib/aarch64-linux-gnu/ld-2.24.so
 rm -rf etc
 cp ../Dockerfile .
 docker build --tag cscix65g/swift-runtime:arm64-latest .
